7b85751fbf0f70657d11bd2b917642d4f62fa72b
hannahhannah20 <cc221009@fhstp.ac.at>
2025-01-20 13:33:44 +0100

    Adding kafka consumer into index

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const {connectToMySql} = require('./services/mysqlService');
 const {connectToMongo} = require('./services/mongoDBService')
+const {startConsumer} = require("./kafka/consumer");
 
 const server = http.createServer((req, res) => {
   res.end('Server is running');
@@ -9,6 +10,7 @@ const server = http.createServer((req, res) => {
 async function startApp() {
     await connectToMySql();
     await connectToMongo();
+    await startConsumer();
     server.listen(3000, () => {
         console.log('Server is running on port 3000');
     });
