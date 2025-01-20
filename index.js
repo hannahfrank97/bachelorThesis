64902758ec87a34f3364723a1ff969ff930e7cdf
hannahhannah20 <cc221009@fhstp.ac.at>
2025-01-20 12:36:09 +0100

    Adding services into index file

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const http = require('http');
+const {connectToMySql} = require('./services/mysqlService');
+const {connectToMongo} = require('./services/mongoDBService')
 
 const server = http.createServer((req, res) => {
-  res.end('Hello, World!');
+  res.end('Server is running');
 });
 
-server.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
+async function startApp() {
+    await connectToMySql();
+    await connectToMongo();
+    server.listen(3000, () => {
+        console.log('Server is running on port 3000');
+    });
+}
+
+startApp().then(() => console.log('App started successfully'));
