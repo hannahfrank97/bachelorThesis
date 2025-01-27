36c45df010f723e1aed6616adca0f65775702106
hannahhannah20 <cc221009@fhstp.ac.at>
2025-01-27 20:50:23 +0100

    Updating index

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
-const http = require('http');
 const {connectToMySql} = require('./services/mysqlService');
-const {connectToMongo} = require('./services/mongoDBService')
-const {startConsumer} = require("./kafka/consumer");
+const {connectToMongo} = require('./services/mongodbService');
+const {startConsumer} = require("./consumer/consumer");
 
-const server = http.createServer((req, res) => {
-  res.end('Server is running');
-});
+const app = require('./consumer/service');
 
 async function startApp() {
     await connectToMySql();
     await connectToMongo();
     await startConsumer();
-    server.listen(3000, () => {
+    app.listen(3000, () => {
         console.log('Server is running on port 3000');
     });
 }
